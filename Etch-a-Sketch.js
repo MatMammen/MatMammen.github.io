69be9b339e70bd95cbefa564d0855bd1b0e70059
MatMammen <mathew1997@gmail.com>
2020-08-14 22:58:41 -0400

    fixed the sizing issues, fixed the aesthetics (CSS), resize added,and color picker added

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,84 +1,84 @@
 var gridContainer = document.querySelector(".grid-container");
 addGridChildTotal(16, 1);
 var gridItems = document.querySelectorAll('.grid-item');
+colorChange();
+reload();
+resize();
 
-console.log(gridItems);
-
-gridItems.forEach((Item) => {
-    Item.addEventListener('mouseover',() => {
-        Item.style.background = 'green'; 
-    });
-    Item.addEventListener('touchmove',() => {
-        Item.style.background = 'green'; 
-    });
-});
-
-const reload = document.createElement('button');
-reload.textContent = 'Press me to restart!';
-reload.style.fontSize = '100px';
-document.body.appendChild(reload);
-
-reload.addEventListener('click', () => {
-    let reloadPrompt = prompt("How many squares per size on the new grid?(up to 29)"); 
-    addGridChildTotal(reloadPrompt, 0);
-    console.log(gridItems);
-    
-});
-
-const red = document.createElement('button');
-red.textContent = 'Press me change color to red!';
-red.style.fontSize = '100px';
-document.body.appendChild(red);
-red.addEventListener('click', () => {
-    gridItems.forEach((Item) => {
-        Item.addEventListener('mouseover',() => {
-            Item.style.background = 'red'; 
-        });
-        Item.addEventListener('touchmove',() => {
-            Item.style.background = 'red'; 
-        });
-    })
-});
-
-const blue = document.createElement('button');
-blue.textContent = 'Press me change color to blue!';
-blue.style.fontSize = '100px';
-document.body.appendChild(blue);
-blue.addEventListener('click', () => {
-    gridItems.forEach((Item) => {
-        Item.addEventListener('mouseover',() => {
-            Item.style.background = 'blue'; 
-        });
-        Item.addEventListener('touchmove',() => {
-            Item.style.background = 'blue'; 
-        });
-    })
-});
-function addGridChildTotal(num, num2) {
-    if (num2 == 0) {
-        gridItems.forEach((Item) => {
-            gridContainer.removeChild(Item)
-        gridItems = document.querySelectorAll(".grid-item");
-        })
-    }
-    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`
+function addGridChildTotal(num) {
+    deleteGrid();
     for (let i = 0; i < (num * num); i++) {
         addGridChild();
     }
+    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`
     gridItems = document.querySelectorAll(".grid-item");
     gridItems.forEach((Item) => {
         Item.addEventListener('mouseover',() => {
-            Item.style.background = 'green'; 
+            Item.style.background = 'black';
         });
         Item.addEventListener('touchmove',() => {
-            Item.style.background = 'green'; 
+            Item.style.background = 'black'; 
         });
     });
     
+}
+function deleteGrid() {
+    let theGrid = document.querySelector(".grid-container");
+    while (theGrid.hasChildNodes()) {
+        theGrid.removeChild(theGrid.firstChild)
+    }
 }
 function addGridChild() {
     const gridItem = document.createElement('div');
     gridItem.className = "grid-item";
-    gridContainer.appendChild(gridItem);
+    document.getElementById("grid-container").appendChild(gridItem);
 }
+function colorChange() { 
+    let colorPicker = document.createElement('input');
+    colorPicker.type = 'color';
+    colorPicker.className = 'color-picker';
+    document.body.appendChild(colorPicker);
+
+    let colorButton = document.createElement('button');
+    colorButton.textContent = 'Press me to change to chosen color!' ;
+    colorButton.className = 'color-picker-confirm';
+    document.body.appendChild(colorButton);
+    colorButton.addEventListener('click', () => {
+        gridItems.forEach((Item) => {
+            Item.addEventListener('mouseover',() => {
+                Item.style.background = `${colorPicker.value}`; 
+            });
+            Item.addEventListener('touchmove',() => {
+                Item.style.background = `${colorPicker.value}`; 
+            });
+        })
+    })
+}
+function reload() {
+    const reload = document.createElement('button');
+    reload.textContent = 'Restart';
+    reload.className = 'restart';
+    document.body.appendChild(reload);
+
+    reload.addEventListener('click', () => {
+        gridItems.forEach((Item) => {
+            Item.style.background = "white";
+        })
+    })
+}
+function resize() {
+    const resize = document.createElement('button');
+    resize.textContent = 'Resize';
+    resize.className = 'resize';
+    document.body.appendChild(resize);
+    let resizePrompt = document.createElement('input')
+    resizePrompt.className = 'text-box';
+    resizePrompt.placeholder = "How many blocks per side?"
+    document.body.appendChild(resizePrompt);
+    resize.addEventListener('click', () => {
+        addGridChildTotal(resizePrompt.value);
+    })
+}
+
+   
